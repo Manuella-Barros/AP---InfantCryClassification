22c7a6285f84886055ce3078e05ebe592fa5519d
Manuella-Barros <manubarrossa@gmail.com>
2025-07-25 17:19:16 -0300

    modelo simples mlp para teste

--- models/mlp.py
+++ models/mlp.py
@@ -2,40 +2,66 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import GlobalAveragePooling1D
 from keras.callbacks import EarlyStopping
+from keras.utils import pad_sequences
+import sklearn.preprocessing as sk_preprocessing
 
-# MLP (Multilayer Perceptron) é  um modelo simples, rápido de treinar
-# A softamx serve para a predição de classes, onde a saída é uma probabilidade entre as classes
-# A função de ativação ReLU (Rectified Linear Unit) é uma escolha comum para camadas ocultas, pois ajuda a evitar o problema do gradiente desaparecendo e permite que o modelo aprenda relações não lineares complexas.
+def mlp(X_train, y_train, X_val, y_val):
+    print("Iniciando o treinamento do modelo MLP...")
 
-model = Sequential([ # Cria uma rede neural sequencial, camada por camada.
-    Dense(128, activation='relu', input_shape=(302, 39)),
-    Dense(64, activation='relu'),
-    Dense(32, activation='relu'),
-    GlobalAveragePooling1D(), # nos passos anteriores a predição é feita para cada frame, mas no final queremos uma única predição por áudio, então usamos o GlobalAveragePooling1D para reduzir a dimensão dos dados.
-    Dense(5, activation='softmax') # probabilidade entre as 5 classes de saída
-])
+    ## PADRONIZA O TAMANHO OS DADOS DE ENTRADA =================================
+    x_train_norm, y_train_norm, x_val_norm, y_val_norm = normalize_data(X_train, y_train, X_val, y_val)
 
-# Summary mostra um resumo da arquitetura do modelo, incluindo o número de parâmetros treináveis, não treináveis e saida de cada camada.
-model.summary()
+    ## CRIA O MODELO SEQUENCIAL, que é uma pilha linear de camadas. ============
+    model = Sequential([
+        Dense(128, activation='relu', input_shape=(302, 39)),
+        Dense(64, activation='relu'),
+        Dense(32, activation='relu'),
+        GlobalAveragePooling1D(),
+        Dense(5, activation='softmax')
+    ])
 
-# Compila o modelo, especificando: 
-# Otimizador -> Adam (Algoritmo que ajusta os pesos da rede neural para minimizar o erro),
-# Função de perda -> sparse_categorical_crossentropy,
-# Métricas a serem monitoradas durante o treinamento -> 'accuracy' (precisão).
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    print("Resumo do modelo:")
+    model.summary()
 
-# É um recurso do Keras que para automaticamente o treinamento do modelo quando a métrica que você escolheu para de melhorar depois de um certo número de épocas.
-early_stop = EarlyStopping(
-    monitor='val_loss', 
-    patience=5, 
-    restore_best_weights=True
-)
+    ## COMPILA O MODELO =======================================================
+    print("Compilando o modelo...")
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(
-    X_train, 
-    y_train, 
-    validation_data=(X_val, y_val), 
-    epochs=30, 
-    batch_size=32,
-    callbacks=[early_stop]
-)
+    ## EARLY STOPPING ==========================================================
+    early_stop = EarlyStopping(
+        monitor='val_loss', 
+        patience=3, 
+        restore_best_weights=True
+    )
+
+    ## TREINA O MODELO =========================================================
+    print("Treinando o modelo...")
+    history = model.fit(
+        x_train_norm, 
+        y_train_norm, 
+        validation_data=(x_val_norm, y_val_norm), 
+        epochs=10,
+        batch_size= 32,
+        callbacks=[early_stop]
+    )
+    print("history:", history.history)
+
+    ## SALVA O MODELO =========================================================
+    # model.save("mlp_model.keras")
+
+    ## CARREGA O MODELO =======================================================
+    # model = load_model("mlp_model.keras")
+
+def normalize_data(X_train, y_train, X_val, y_val):
+    ## PADRONIZA O TAMANHO DOS VETORES DE ENTRADA ==============================
+    max_len = 302
+
+    X_train = pad_sequences(X_train, maxlen=max_len, padding='post', dtype='float32')
+    X_val = pad_sequences(X_val, maxlen=max_len, padding='post', dtype='float32')
+
+    ## PADRONIZA AS CLASSES ====================================================
+    encoder = sk_preprocessing.LabelEncoder()
+    y_train = encoder.fit_transform(y_train)
+    y_val = encoder.transform(y_val)
+
+    return X_train, y_train, X_val, y_val
