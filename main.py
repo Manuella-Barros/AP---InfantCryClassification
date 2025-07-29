cb82163141071dbdb31cec58d0ac97095087b837
Manuella-Barros <manubarrossa@gmail.com>
2025-07-29 10:47:31 -0300

    adiciona o modelo cnn na main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from data_augmentation import augmented_train_dataset
 from mfcc.mfcc import get_mfcc_from_file_list, get_mfcc_from_file
 from models.mlp import mlp
+from models.cnn import cnn
 
 ## PEGA E AJUSTA OS DADOS INICIAIS =============================================
 # Caminho para a pasta principal contendo subpastas (cada subpasta é uma classe)
@@ -27,7 +28,7 @@ x_train, x_test, y_train, y_test = train_test_split(file_paths, labels, test_siz
 
 ## APLICA O DATA AUGMENTATION NOS AUDIOS DE TREINAMENTO ========================
 # # Gera os dados aumentados e atualiza os conjuntos de treino
-x_train, y_train = augmented_train_dataset(x_train, y_train)
+# x_train, y_train = augmented_train_dataset(x_train, y_train)
 
 # print(f"file_paths: {file_paths}")
 # print(f"labels: {labels}")
@@ -43,4 +44,6 @@ x_test_mfcc = get_mfcc_from_file_list(x_test)
 print(f"MFCCs de treino: {len(x_train_mfcc)}")
 print(f"MFCCs de teste: {len(x_test_mfcc)}")
 
-mlp(x_train_mfcc, y_train, x_test_mfcc, y_test)
+## TREINA OS MODELOS ===========================================================
+# mlp(x_train_mfcc, y_train, x_test_mfcc, y_test)
+value_predict, y_val_norm = cnn(x_train_mfcc, y_train, x_test_mfcc, y_test)
